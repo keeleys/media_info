fafd2890d36a932b25fb7a2c54f42351d7e5e0d3
keeleys <keeley.jun@qq.com>
2017-08-02 15:35:33 +0800

    update

--- src/main/java/cc/sitec/MediaKit.java
+++ src/main/java/cc/sitec/MediaKit.java
@@ -3,6 +3,7 @@ package cc.sitec;
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
+import java.text.ParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,20 +11,28 @@ import java.util.regex.Pattern;
  * Created by keeley on 2017/2/23.
  */
 public class MediaKit {
-    private static final String regexDuration = "Duration: (.*?), start: (.*?), bitrate: (\\d*) kb\\/s";
-    private static final String regexVideo = "Video: (.*?), (.*?), (.*?)[,\\s]";
-    private static final String regexAudio = "Audio: (\\w*), (\\d*) Hz";
+//    private static final String regexDuration = "Duration: (.*?), start: (.*?), bitrate: (\\d*) kb\\/s";
+//    private static final String regexVideo = "Video: (.*?), (.*?), (.*?)[,\\s]";
+//    private static final String regexAudio = "Audio: (\\w*), (\\d*) Hz";
 
-    public String getDuration(String file) throws IOException {
+    public long getDuration(String file) throws Exception {
         String info = getInfo(file);
-        Pattern p= Pattern.compile(regexDuration);
-        Matcher m=p.matcher(info);
-        if( m.find()) return m.group(1);
-        return null;
+        Pattern p2 = Pattern.compile("Duration: (\\d\\d):(\\d\\d):(\\d\\d)\\.(\\d)", 2);
+
+        Matcher m=p2.matcher(info);
+        if( m.find()) {
+            long hours = (long)Integer.parseInt(m.group(1));
+            long minutes = (long)Integer.parseInt(m.group(2));
+            long seconds = (long)Integer.parseInt(m.group(3));
+            long dec = (long)Integer.parseInt(m.group(4));
+            long duration = dec * 100L + seconds * 1000L + minutes * 60L * 1000L + hours * 60L * 60L * 1000L;
+            return duration;
+        }
+        return -1;
     }
 
     private String getInfo(String file) throws IOException {
-        ProcessBuilder pb = new ProcessBuilder("/usr/local/bin/ffmpeg" , "-i", file);
+        ProcessBuilder pb = new ProcessBuilder("ffmpeg" , "-i", file);
         pb.redirectErrorStream(true);
         Process process = pb.start();
         return IOUtils.toString(process.getInputStream(), "utf-8");
--- src/test/java/TestMediaKit.java
+++ src/test/java/TestMediaKit.java
@@ -1,17 +1,15 @@
 import cc.sitec.MediaKit;
 import org.junit.Test;
 
-import java.io.IOException;
-
 /**
  * Created by keeley on 2017/3/4.
  */
 public class TestMediaKit {
 
     @Test
-    public void teseInfo() throws IOException {
+    public void teseInfo() throws Exception {
         String file = "/Users/keeley/Movies/test.mp4";
         MediaKit kit = new MediaKit();
-        System.out.println(kit.getDuration(file));
+        System.out.println(kit.getDuration(file)/1000);
     }
 }
